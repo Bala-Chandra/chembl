0861d14136784793e42f0bc681c300c815cf34a9
Bala chandra <bala.reddy.ece@gmail.com>
2026-02-04 17:18:50 +0530

    cors issue

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // 👈 frontend
+    credentials: true, // 👈 REQUIRED for cookies
+  });
+  await app.listen(3000);
 }
 void bootstrap();
